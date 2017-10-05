640719ba1e8136bedc5bc2782b2aa7b6d4cd1219
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2017-10-05 08:31:14 +0300

    Move to Data Driven controller for all cards

--- Redux Workshop Sample/AllCardsViewController.swift
+++ Redux Workshop Sample/AllCardsViewController.swift
@@ -1,18 +1,28 @@
 import UIKit
 
 class AllCardsViewController: UITableViewController {
+    struct Props {
+        let cards: [String]
+    }
+    
+    var props: Props = Props(cards: []){
+        didSet { tableView.reloadData() }
+    }
+    
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return props.cards.count
     }
     
     override func tableView(_ tableView: UITableView,
                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        precondition(props.cards.indices.contains(indexPath.row))
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "card-details") else {
             fatalError("Cannot get card cell from \(tableView)")
         }
         
-        cell.textLabel?.text = "Some card title"
+        cell.textLabel?.text = props.cards[indexPath.row]
         
         return cell
     }
--- Redux Workshop Sample/AppDelegate.swift
+++ Redux Workshop Sample/AppDelegate.swift
@@ -2,11 +2,24 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        let allCards = navigationController.viewControllers.first as! AllCardsViewController
+        
+        allCards.props = .init(
+            cards: ["Test 1",
+                    "Test 2",
+                    "Test 3" ]
+        )
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 }
