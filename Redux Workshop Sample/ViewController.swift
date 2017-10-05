840bbcb6101064bdc61c8e3a25678075308ac2ba
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2017-10-05 07:03:05 +0300

    Initial Commit

---
+++ Redux Workshop Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Redux Workshop Sample
+//
+//  Created by Alexey Demedetskii on 10/5/17.
+//  Copyright © 2017 Alexey Demedeckiy Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
